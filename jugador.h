afca9ecc9c0e8c9d047dcc9fa4f20ab2183d0ede
caviedes93 <andres_luis93@hotmail.com>
2013-07-07 10:07:22 -0500

    actualización 2 de la clase ranking

--- jugador.h
+++ jugador.h
@@ -5,6 +5,10 @@ class Jugador
 {
 public:
     Jugador();
+    QString getNombre();
+    int getPuntaje();
+    void setNombre(QString n);
+    void setPuntaje(int p);
  private:
     int puntaje;
     String nombre;
--- ranking.cpp
+++ ranking.cpp
@@ -1,21 +1,22 @@
 #include "ranking.h"
 #include "ui_ranking.h"
+#include "jugador.h"
 
 Ranking::Ranking(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Ranking)
 {
     ui->setupUi(this);
-    QList ordenado,ranking;
+    QList<Jugador> ordenado;
     ordenado=ordenarRanking(cargarRanking());
     model = new QStandardItemModel(ordenado.size(),2,this);
     model->setHorizontalHeaderItem(0,new QStandardItem("Nombre"));
      model->setHorizontalHeaderItem(1,new QStandardItem("Tiempo"));
     for(int i=0; i<ordenado.size();i++){
         QModelIndex index1 = model->index(i,0,QModelIndex());
-        model->setData(index1,ordenado.at(i));
+        model->setData(index1,ordenado.at(i).getNombre());
         QModelIndex index2 = model->index(i,1,QModelIndex());
-        model->setData(index2,ordenado.at(i));
+        model->setData(index2,ordenado.at(i).getPuntaje());
     }
     ui->tableView->setModel(model);
 }
@@ -25,18 +26,18 @@ Ranking::~Ranking()
     delete ui;
 }
 
-QList Ranking:: cargarRanking(){
-    QList ranking;
+QList<Jugador> Ranking:: cargarRanking(){
+    QList<Jugador> ranking;
     retturn ranking;
 }
 
-QList Ranking:: ordenarRanking(QList ranking){
-    QList ordenado;
+QList<Jugador> Ranking:: ordenarRanking(QList<Jugador> ranking){
+    QList<Jugador> ordenado;
     int i,pos;
     while(ranking.size()!=0){
         pos=0;
         for(i=1;i<ranking.size();i++){
-            if(ranking.at(pos)>ranking.at(i))
+            if(ranking.at(pos).getPuntaje()>ranking.at(i).getPuntaje())
                 pos=i;
         }
         ordenado.append(ranking.at(pos));
