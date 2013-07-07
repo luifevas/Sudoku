09dcfa0641f877c3621e997b0bdef2f780fb598d
caviedes93 <andres_luis93@hotmail.com>
2013-07-07 11:07:45 -0500

    finalizado la parte de ranking

--- ranking.cpp
+++ ranking.cpp
@@ -2,6 +2,7 @@
 #include "ui_ranking.h"
 #include <QFile>
 #include <QTextStream>
+
 Ranking::Ranking(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Ranking)
@@ -13,10 +14,11 @@ Ranking::Ranking(QWidget *parent) :
     model->setHorizontalHeaderItem(0,new QStandardItem("Nombre"));
      model->setHorizontalHeaderItem(1,new QStandardItem("Tiempo"));
     for(int i=0; i<ordenado.size();i++){
+        Jugador j= ordenado.at(i);
         QModelIndex index1 = model->index(i,0,QModelIndex());
-        model->setData(index1,ordenado.at(i).getNombre());
+        model->setData(index1,j.getNombre());
         QModelIndex index2 = model->index(i,1,QModelIndex());
-        model->setData(index2,ordenado.at(i).getPuntaje());
+        model->setData(index2,j.getPuntaje());
     }
     ui->tableView->setModel(model);
 }
@@ -35,8 +37,8 @@ QList<Jugador> Ranking:: cargarRanking(){
 
     while(!in.atEnd()){
         QString linea= in.readLine();
-        Jugador tmp= new Jugador(linea.section(',',0,0),linea.section(',',1,1).toInt());
-        ranking.append(tmp);
+        Jugador *tmp= new Jugador(linea.section(',',0,0),linea.section(',',1,1).toInt());
+        ranking.append(*tmp);
 
     }
 
@@ -51,7 +53,9 @@ QList<Jugador> Ranking:: ordenarRanking(QList<Jugador> ranking){
     while(ranking.size()!=0){
         pos=0;
         for(i=1;i<ranking.size();i++){
-            if(ranking.at(pos).getPuntaje()>ranking.at(i).getPuntaje())
+            Jugador j1=ranking.at(pos);
+            Jugador j2=ranking.at(i);
+            if(j1.getPuntaje()>j2.getPuntaje())
                 pos=i;
         }
         ordenado.append(ranking.at(pos));
